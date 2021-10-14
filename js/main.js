c3c4f2f1a8686cf13cd083f3d77fc4bec176e3ca
jeetnarang <jeetnarang86@gmail.com>
2021-10-14 13:37:24 +0530

    init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const Responsive = {
+  320: {
+    items: 1,
+    nav: true,
+  },
+
+  560: {
+    items: 2,
+    nav: true,
+  },
+
+  960: {
+    items: 3,
+    nav: true,
+  },
+};
+
 $(document).ready(function () {
   $nav = $(".nav");
   $toggleCollapse = $(".toggle-collapse");
@@ -8,15 +25,30 @@ $(document).ready(function () {
   });
 
   //owl-carousel for blog
+
   $(".owl-carousel").owlCarousel({
     loop: true,
+    margin: 10,
     autoplay: true,
     autoplayTimeout: 3000,
+    autoplayHoverPause: true,
     dots: false,
     nav: true,
     navText: [
       $(".owl-navigation .owl-nav-prev"),
       $(".owl-navigation .owl-nav-next"),
     ],
+    ResponsiveClass: true,
+  });
+
+  // click to scroll up
+
+  $(".move-up span").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
   });
 });
